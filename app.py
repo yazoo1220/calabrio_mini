f8e3c90b2b027997c6e9a480ae15baadca47cea3
yazoo1220 <podman1220@hotmail.com>
2023-09-09 03:11:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ async def fetch_people_data(base_url, api_token, business_units_to_exclude):
 def main():
     st.title("My Streamlit App")
     with st.sidebar:
-        base_url = st.input("your_api_base_url")
-        api_token = st.input("your_api_token")
-        business_units_to_exclude = st.input("").split(',')  # Replace with your exclusion list
+        base_url = st.taxt_input("your_api_base_url")
+        api_token = st.text_input("your_api_token")
+        business_units_to_exclude = st.text_input("").split(',')  # Replace with your exclusion list
     if st.button("Run"):      
         # Use asyncio to run the asynchronous code
         loop = asyncio.get_event_loop()
